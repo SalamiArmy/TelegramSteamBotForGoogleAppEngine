d65c0ea3c096bc8534d42c702a834aafd43c3ca5
SalamiArmy <ultrakiff@gmail.com>
2016-07-09 15:25:13 +0200

    workaround error parsing message text

--- commands/getgame.py
+++ commands/getgame.py
@@ -16,8 +16,8 @@ def run(bot, chat_id, user, message):
         bypassAgeGate.addheaders.append(('Cookie', 'birthtime=578390401'))
         code = bypassAgeGate.open(steamGameLink).read()
         gameResults = steam_game_parser(code, steamGameLink)
-        bot.sendMessage(chat_id=chat_id, text=gameResults.replace(':',''),
-                        disable_web_page_preview=True, parse_mode='Markdown')
+        bot.sendMessage(chat_id=chat_id, text=gameResults,
+                        disable_web_page_preview=True)
         return True
     else:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') + \
