8f7c4e1999a72843a240f73c51f6953b5fdb5641
SalamiArmy <ultrakiff@gmail.com>
2016-07-27 15:40:52 +0200

    add mashape key

--- commands/getimgur.py
+++ commands/getimgur.py
@@ -9,9 +9,11 @@ def run(bot, keyConfig, chat_id, user):
     client_id = keyConfig.get('Imgur', 'CLIENT_ID')
     client_secret = keyConfig.get('Imgur', 'CLIENT_SECRET')
     client = ImgurClient(client_id, client_secret)
+    client.mashape_key = keyConfig.get('Mashape', 'API_KEY')
     items = client.subreddit_gallery(subreddit='pcmasterrace',
                                      sort='top',
-                                     window='day')
+                                     window='day',
+                                     page=1)
     if len(items) > 0:
         bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
         bot.sendPhoto(chat_id=chat_id,
