da3ac2d5888ae2760a7f063879017fb0105da608
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-03-02 09:16:50 +0200

    still trying to get /resethotgames to work

--- commands/resethotgames.py
+++ commands/resethotgames.py
@@ -4,15 +4,15 @@ import string
 import sys
 from google.appengine.ext import ndb
 
-from commands.addhotgame import SetHotGame
+from commands.addhotgame import HotGamesDataStore
 
 
 def run(bot, chat_id, user):
     try:
-        OldValue = SetHotGame.getHotGames(chat_id)
+        OldValue = HotGamesDataStore.getHotGames(chat_id)
         if OldValue == '':
             OldValue = 'blank'
-        SetHotGame.resetHotGames(chat_id)
+        HotGamesDataStore.resetHotGames(chat_id)
         bot.sendMessage(chat_id=chat_id, text='Chat ' + str(chat_id) + ' was:\n' + OldValue + '\nHas been reset.')
     except:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
