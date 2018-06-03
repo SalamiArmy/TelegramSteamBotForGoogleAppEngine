1850b23517e3021e76c31e78ff6843cddc135ca1
SalamiArmy <ultrakiff@gmail.com>
2018-06-03 16:21:29 +0200

    references broken by rename refactor

--- telegram_commands/unwatchpopgames.py
+++ telegram_commands/unwatchpopgames.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from commands.watchpopgames import unwatch
+from telegram_commands.watchpopgames import unwatch
 
 def run(bot, chat_id, user, keyConfig='', message=''):
     unwatch(bot, chat_id)
--- telegram_commands/unwatchtopgames.py
+++ telegram_commands/unwatchtopgames.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from commands.watchtopgames import unwatch
+from telegram_commands.watchtopgames import unwatch
 
 def run(bot, chat_id, user, keyConfig='', message=''):
     unwatch(bot, chat_id)
